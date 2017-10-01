639a6870090bff3568588d81b8dbe0ed6ff4194c
UmbraGrim <houlemack@yahoo.com>
2017-10-01 09:29:10 -0400

    ProtoUpload
    Uploading Prototype files

---
+++ PongProj/Assets/PlayerPaddle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPaddle : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var z = Input.GetAxis("Vertical") * /*Time.deltaTime */ 10.0f;
+        transform.Translate(Vector3.up *Time.deltaTime);
+	}
+}
