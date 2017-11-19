714364b9b8f4d1942cb15149d013266bbb177fb8
IrishJohn101 <32434562+IrishJohn101@users.noreply.github.com>
2017-11-19 13:47:55 -0500

    Shit changed
    I added a script to the ball so we could change the speed I think.

---
+++ PongProj/Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public GameObject ball;
+    
+    // Use this for initialization
+	void Start () {
+        ball.GetComponent<Rigidbody>().AddForce(Vector3.right * 10.0f);
+        
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
