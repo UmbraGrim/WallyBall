06f5035763c3b9aee6c3cb623923f8c316ded287
UmbraGrim <houlemack@yahoo.com>
2017-12-05 11:02:55 -0500

    Ball Update
    Began to work on creating ball acceleration. Currently not finished.

--- PongProj/Assets/Scripts/Ball.cs
+++ PongProj/Assets/Scripts/Ball.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+    //creates a GameObject in the inspector to drop a game object in, giving it the properties below
     public GameObject ball;
-    
+
+    //SerializeField variables, private so only accessable by this class. Serialize field allows them to be modified in inspector
+    [SerializeField]
+    private float ballSpeed;// = 10.0f;
+    [SerializeField]
+    private float speedIncrease;
+    [SerializeField]
+    private float maxSpeed;
+
+
     // Use this for initialization
 	void Start () {
-        ball.GetComponent<Rigidbody>().AddForce(Vector3.right * 10.0f);
-        
-
+        ball.GetComponent<Rigidbody>().AddForce(Vector3.right * ballSpeed);
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //updateBallSpeed();
+    }
+
+    /*//updates in a fixed framerate
+    private void FixedUpdate()
+    {
+        //updateBallSpeed();
+    }
+
+    void updateBallSpeed()
+    {
+        //ballSpeed = 1.0f;
+        while (ballSpeed <= 30.0f)
+        {
+           ball.GetComponent<Rigidbody>().AddForce(Vector3.right * ballSpeed);
+           ballSpeed+=speedIncrease;// = (ballSpeed + speedIncrease);
+        }         
+    }*/
 }
